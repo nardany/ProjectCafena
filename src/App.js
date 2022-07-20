62a6caf41b7aa5822b0041d415a364ba24b01cfb
NarDany <narekdanielian04@gmail.com>
2022-07-20 18:07:10 +0400

    home page create

--- src/App.js
+++ src/App.js
@@ -13,43 +13,43 @@ import Contact from "./Components/contact"
 function App() {
   return (
     <div className="App">
-<BrowserRouter>
-      <div className="App">
-        <div className="NavBar">
-          <div className='NavBar1'>
-          <Link to="/">Home</Link>
-          <Link to="/about">About </Link>
-          <Link to="/menu">Contact</Link>
-          <Link to="/reservation">Reservation</Link>
+      <BrowserRouter>
+        <div className="App">
+          <div className="NavBar">
+            <div className="NavBar1">
+              <Link to="/">Home</Link>
+              <Link to="/about">About </Link>
+              <Link to="/menu">Contact</Link>
+              <Link to="/reservation">Reservation</Link>
+            </div>
+            <div className="logo">
+              <img src={Logo} />
+            </div>
+            <div className="NavBar2">
+              <Link to="/pages">Pages</Link>
+              <Link to="/shop">Shop </Link>
+              <Link to="/contact">Contact</Link>
+            </div>
           </div>
-          <div className='logo'>
-          <img src={Logo}/>
-          </div>
-          <div className='NavBar2'>
-          <Link to="/pages">Pages</Link>
-          <Link to="/shop">Shop </Link>
-          <Link to="/contact">Contact</Link>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/reservation" element={<Reservation />} />
+            <Route path="/pages" element={<Pages />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h1>Not Found</h1>
+                </div>
+              }
+            />
+          </Routes>
         </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/reservation" element={<Reservation />} />
-          <Route path="/pages" element={<Pages />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route
-            path="*"
-            element={
-              <div>
-                <h1>Not Found</h1>
-              </div>
-            }
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,16 +1,101 @@
-import "./Css files/home.css"
-export default function (){
-    return(
-        <div className="homepage">
-            <div className="text">
-                    <h1 className="homeh1">ROSTED COFFEE
-                        <br />
-                            FOR YOU
-                    </h1>
-                    <p className="homep1">The coffee is brewed by first roasting the green coffee beans over hot 
-                        <br />    
-                       coals in a brazier. given an opportunity to sample.</p>
-            </div>
+import "./Css files/home.css";
+import About from "./../Components/about"
+import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+
+export default function () {
+  return (
+    <div className="home">
+      <div className="homepage">
+        <div className="text">
+          <h1 className="homeh1">
+            ROSTED COFFEE
+            <br />
+            FOR YOU
+          </h1>
+          <p className="homep1">
+            The coffee is brewed by first roasting the green coffee beans over
+            hot
+            <br />
+            coals in a brazier. given an opportunity to sample.
+          </p>
         </div>
-    )
+      </div>
+      <div className="page2">
+        <div className="photo1">
+          <p className="number">01</p>
+          <h2 className="title">Best Coffee flavour</h2>
+          <p className="textphoto">
+            Curabitur semper erat a lacusey sedn <br />
+            consequat, sit ametey.
+          </p>
+        </div>
+        <div className="photo2">
+          <p className="number">02</p>
+          <h2 className="title">Place to Get Lost</h2>
+          <p className="textphoto">
+            Curabitur semper erat a lacusey sedn <br />
+            consequat, sit ametey.
+          </p>
+        </div>
+        <div className="photo3">
+          <p className="number">03</p>
+          <h2 className="title">Proper Roesting</h2>
+          <p className="textphoto">
+            Curabitur semper erat a lacusey sedn <br />
+            consequat, sit ametey.
+          </p>
+        </div>
+      </div>
+      <div className="page3">
+        <div className="page3img"></div>
+        <div className="page3form">
+            <p className="page3p1">OUR HISTORY</p>
+            <br />
+            <br />
+            <h1 className="page3title">CREATE A NEW STORY WITH US</h1>
+            <p className="page3p2">Mauris rhoncus orci in imperdiet placerat. Vestibulum euismod <br/>
+            nisl suscipit ligula volutpat, a feugiat urna maximus. Cras massa <br/>
+            nibhtincidunt.</p>
+            <br />
+            <br />
+            <p className="page3p3">Donec et nibh maximus, congue est eu, mattis nunc. Praesent ut <br/>
+            quam quis quam venenatis fringilla. Morbi vestibulum id tellus <br/> 
+            mmodo mattis. Aliquam erat volutpat.</p>
+        </div>
+      </div>
+      <div className="page4">
+            <div className="page4text">
+                <p className="page4p">MENU</p> 
+                <br />
+                 <p className="page4tittle">CAFENA POPULAR MENU</p>
+            </div>
+            <div className="page4menu">
+                <div className="page4coffee1">
+                        <div className="coffee1">
+
+                        </div>
+                        <div className="coffee2">
+
+                        </div>
+                </div>
+                <div className="page4coffee2">
+                        <div className="coffee3">
+
+                        </div>
+                        <div className="coffee4">
+
+                        </div>
+                </div>
+                <div className="page4coffee3">
+                        <div className="coffee5">
+
+                        </div>
+                        <div className="coffee6">
+                            
+                        </div>
+                </div>
+            </div>
+      </div>
+    </div>
+  );
 }
