b59469568bec9d50ff4c38239bb895eec0b2f2b1
NarDany <narekdanielian04@gmail.com>
2022-07-28 14:50:36 +0400

    about page

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -1,39 +1,123 @@
 import "../About/About.css";
-import line from "../../Assets/gic.png"
-import menimage from "../../Assets/AboutPage/aboutpagemen.png"
-import {FaCheck} from 'react-icons/fa';
-    
-export default function (){
-    return(
-        <div className="AboutPage">
-            <div className="page1about">
-                <img src={line} alt="line" className="linepage1" />
-                <div className="textpage8">
-                <h1>ABOUT CAFENA</h1>
-                <p>HOME/<i className="page1i"> ABOUT US</i></p>
-                </div>
-                <img src={line} alt="line" className="linepage2" />
+import line from "../../Assets/gic.png";
+import coffeeabout from "../../Assets/AboutPage/aboutpagecoffee.png";
+import coffeemen from "../../Assets/AboutPage/aboutpagemen.png";
+import { FaCheck } from "react-icons/fa";
+import icon1 from "../../Assets/AboutPage/icon1.png"
+import icon2 from "../../Assets/AboutPage/icon2.png"
+import {FaLongArrowAltRight} from "react-icons/fa";
+
+export default function () {
+  return (
+    <div className="AboutPage">
+      <div className="page1about">
+        <img src={line} alt="line" className="linepage1" />
+        <div className="textpage8">
+          <h1>ABOUT CAFENA</h1>
+          <p>
+            HOME/<i className="page1i"> ABOUT US</i>
+          </p>
+        </div>
+      </div>
+
+      <div className="page2about">
+        <div className="div_1">
+          <div className="section-image">
+            <img src={coffeemen} alt="" className="coffeemen" />
+            <img src={coffeeabout} alt="" className="coffeeabout" />
+          </div>
+        </div>
+        <div className="div_2">
+          <p className="div_2_tittle_">ABOUT CAFENA</p>
+          <h1>
+            ONE OF THE BEST COFFEE HOUSE <br />
+            IN YOUR HOME TOWN
+          </h1>
+          <i>
+            Mauris rhoncus orci in imperdiet placerat. Vestibulum euismod nisl{" "}
+            <br />
+            suscipit ligula volutpat, a feugiat urna maximus. Cras massa <br />
+            nibhtincidunt.
+          </i>
+
+          <ul className="checkul">
+            <li>
+              <span>
+                <i>
+                  <FaCheck />
+                </i>
+              </span>
+             <p> What is Lorem Ipsum Lorem Ipsum is simply.</p>
+            </li>
+            <li>
+              <span>
+                <i>
+                  <FaCheck />
+                </i>
+              </span>
+              <p>
+              Dummy text of the printing text.
+              </p>
+            </li>
+            <li>
+              <span>
+                <i>
+                  <FaCheck />
+                </i>
+              </span>
+              <p>
+              Typesetting industry Lorem Ipsum has been the industry's
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="page3about">
+        <div className="page3text">
+        <p className="page3tittle">WHY CHOOSE US</p>
+        <h1>WE ARE PROVIDE BEST SERVICE <br />
+        IN YOUR CITY</h1>
+        </div>
+        <div className="page3container">
+            <div className="container1 container">
+                    <div className="imgcontainer">
+                    <img src={icon1} alt="" className="icon1 icon" />
+                    </div>
+                    <h2>TESTY COFFEE RECIPE</h2>
+                    <p>Accumsa nfringilla. Morbi vestibulum id <br />
+                    tellus mmodo mattis. Aliquam erat <br />
+                    volutpat. Aenean accumsany.</p>
+                    <div className="buttoncontainer">
+                    <button><FaLongArrowAltRight/></button>
+                    </div>
             </div>
-            <div className="page2about">
-                <div className="div">
-                    <img src={menimage} alt="aboutpagemen" />
-                </div>
-                <div className="div">
-                        <p>ABOUT CAFENA</p>
-                        <h1>ONE OF THE BEST COFFEE HOUSE <br />
-                        IN YOUR HOME TOWN</h1>
-                        <i>Mauris rhoncus orci in imperdiet placerat.
-                        Vestibulum euismod nisl <br />
-                        suscipit ligula volutpat,
-                        a feugiat urna maximus. Cras massa <br />
-                        nibhtincidunt.</i>
-                        <div className="containercheck">
-                            <FaCheck/> <span> What is Lorem Ipsum Lorem Ipsum is simply.</span>
-                            <FaCheck/> <span> Dummy text of the printing text.</span>
-                            <FaCheck/> <span> Typesetting industry Lorem Ipsum has been the industry's</span>
-                        </div>
-                </div>
+            <div className="container2 container">
+            <div className="imgcontainer">
+                    <img src={icon2} alt="" className="icon2 icon" />
+                    </div>
+                    <h2>GREAT LOCATION</h2>
+                    <p>Accumsa nfringilla. Morbi vestibulum id <br />
+                    tellus mmodo mattis. Aliquam erat <br />
+                    volutpat. Aenean accumsany.</p>
+                    <div className="buttoncontainer">
+                    <button><FaLongArrowAltRight/></button>
+                    </div>
+            </div>
+            <div className="container3 container">
+            <div className="imgcontainer">
+                    <img src={icon1} alt="" className="icon3 icon" />
+                    </div>
+                    <h2>PROFESSIONAL CHEF</h2>
+                    <p>Accumsa nfringilla. Morbi vestibulum id <br />
+                    tellus mmodo mattis. Aliquam erat <br />
+                    volutpat. Aenean accumsany.</p>
+                    <div className="buttoncontainer">
+                    <button><FaLongArrowAltRight/></button>
+                    </div>
             </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
